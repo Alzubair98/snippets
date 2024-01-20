2aa5ba4cd9ae1b874d8a2f39768ea0887d217ab1
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-01-21 00:10:18 +0300

    add style to pages

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -16,5 +16,20 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     },
   });
 
-  return <div>{snippet ? snippet.title : notFound()}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">
+          {snippet ? snippet.title : notFound()}{" "}
+        </h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 broder-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
