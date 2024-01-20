89cfdf579f634cf508d3eaae86531e8e649281d7
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-01-20 23:36:03 +0300

    fetch snippet from database

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,22 @@
-export default function SnippetShowPage(props: any) {
-  console.log(props);
-  return <div>show a snippet</div>;
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id: parseInt(props.params.id),
+    },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
 }
