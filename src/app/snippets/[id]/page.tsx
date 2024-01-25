70405e1cf0029f2b044d7b85f1450760c39df565
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-01-25 08:04:26 +0300

    make the app use cach

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -46,3 +46,15 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     </div>
   );
 }
+
+// to add cach to dynamic page
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    };
+  });
+}
